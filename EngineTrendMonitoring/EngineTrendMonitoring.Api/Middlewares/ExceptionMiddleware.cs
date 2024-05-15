1ddce5e49d8198991d5cca57554cdfeac423842f
Fernando Keller <fernando.keller@stgen.com>
2024-05-14 23:27:45 -0300

    Added exception middleware.

---
+++ EngineTrendMonitoring/EngineTrendMonitoring.Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using EngineTrendMonitoring.Shared.Exceptions;
+using EngineTrendMonitoring.Shared.Models.Result;
+using System.Net;
+
+namespace EngineTrendMonitoring.Api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        #region Properties
+        private readonly RequestDelegate _next;
+        #endregion
+
+        #region Constructor
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        #endregion
+
+        #region Public Methods
+
+        #region Invoke Async
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (CustomException ce)
+            {
+                await HandleCustomExceptionAsync(httpContext, ce);
+            }
+            catch (Exception)
+            {
+                await HandleExceptionAsync(httpContext);
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Private Methods
+
+        #region Handle Exception Async
+        private async Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(ResultModel.WithError("Something went wrong"));
+        }
+        #endregion
+
+        #region Handle Custom Exception Async
+        private async Task HandleCustomExceptionAsync(HttpContext context, CustomException customException)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+            await context.Response.WriteAsJsonAsync(customException.ResultModel);
+        }
+        #endregion
+
+        #endregion
+    }
+}
--- EngineTrendMonitoring/EngineTrendMonitoring.Api/Program.cs
+++ EngineTrendMonitoring/EngineTrendMonitoring.Api/Program.cs
@@ -1,4 +1,5 @@
 using EngineTrendMonitoring.Api.Configurations;
+using EngineTrendMonitoring.Api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthorization();
 
